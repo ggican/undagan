e57ed956b44986c40def52db7a2b1c99a156ef12
im-tiket <ikhsan.mahendri@tiket.com>
2024-11-25 09:55:23 +0700

    new update undangan

--- src/App.jsx
+++ src/App.jsx
@@ -28,9 +28,6 @@ function App() {
       behavior: 'smooth',
       block: 'start', // Scroll to the top of the section
     });
-    return () => {
-
-    }
   }, [])
 
   const open = () => {
@@ -43,17 +40,24 @@ function App() {
     }
   };
 
+
   return (
     <>
       <div className="w-full p-0 flex justify-center items-center">
         <div className="w-[100%] md:w-[550px] sm:w-[550px] xs:w-[550px] lg:w-[550px]">
           <section ref={firstSectionRef} id="first" className="w-full h-screen relative">
-            <div className="flower-right z-30 top-0 right-0 w-[30%] rotate-90 absolute inline-flex justify-end">
-              <img className="w-[100%]" src={flowerOne} alt="" />
+            <div className='animate-slide-left'>
+              <div className="flower-right z-30 top-0 right-0 w-[100px] rotate-90 absolute inline-flex justify-end">
+                <img className="w-[100%]" src={flowerOne} alt="" />
+              </div>
             </div>
-            <div className="flower-left z-30 top-0 left-0 w-[30%] absolute inline-flex justify-end">
-              <img className="w-[100%]" src={flowerOne} alt="" />
+            <div className="animate-slide-right">
+              <div className="flower-left z-30 top-0 left-0 w-[100px] absolute inline-flex justify-end ">
+                <img className="w-[100%]" src={flowerOne} alt="" />
+              </div>
             </div>
+
+
             {/* top */}
             <img className="w-full h-screen object-cover" src={imageBackground} alt="" />
             <div className="flower-left z-10 top-0 left-0 [transform:rotateY(180deg)] w-[100%] absolute inline-flex justify-end">
@@ -61,71 +65,79 @@ function App() {
             </div>
             <div className='absolute top-[0] left-[50%] bottom-0 flex flex-col justify-start pt-[20px] items-center w-full [transform:translateX(-50%)] p-[20px]'>
               <div className="w-full relative flex justify-center">
-                <p className='text-eb-garamond text-[#6f3617] pt-[30px] text-[25px] font-bold text-center relative'>
+                <p className='animate-slide-down text-eb-garamond text-[#6f3617] pt-[30px] text-[25px] font-bold text-center relative'>
                   UNDANGAN
-                  <div className="w-[60px] z-50 absolute left-[-80px] top-[20px]">
+                  <div className="w-[60px] z-50 absolute left-[-90px] top-[35px]">
                     <img src={fks} alt="" />
                   </div>
-                  <div className="w-[70px] z-40 absolute right-[-100px] top-[10px]">
+                  <div className="w-[70px] z-50 absolute right-[-100px] top-[25px]">
                     <img src={insanAulia} alt="" />
                   </div>
                 </p>
 
               </div>
-              <p className='text-eb-garamond text-[18px] text-[#6f3617] font-bold text-center'>SEMINAR PARENTING</p>
-              <p className='text-[#6f3617] mt-[10px] text-center font-bold uppercase text-eb-garamond text-[18px]'>
+              <p className='text-eb-garamond text-[18px] text-[#6f3617] font-bold text-center animate-slide-down'>SEMINAR PARENTING</p>
+              <p className='text-[#6f3617] mt-[10px] text-center font-bold uppercase text-eb-garamond text-[18px] animate-slide-down'>
                 Kepada Bapak/ibu/saudara/i
               </p>
-              <p className='text-[#6f3617] mt-[10px] text-center font-normal text-eb-garamond text-[14px]'>
+              <p className='text-[#6f3617] mt-[10px] text-center font-normal text-eb-garamond text-[14px] animate-slide-down animate-slide-down'>
                 Tanpa mengurangi rasa hormat, kami mengundang anda untuk hadir di acara seminar parenting
               </p>
-              <p className='text-[#6f3617]  mt-[10px] text-center font-bold text-eb-garamond text-[18px]'>
-                SDIT Insan Aulia
-              </p>
-              <div className='w-full flex justify-center items-center mt-[10px]'>
+              <div className='w-full flex justify-center items-center mt-[10px] animate-slide-down'>
                 <AvatarElement></AvatarElement>
               </div>
               <div className='flex justify-center items-center  mt-[10px]'>
                 <div className="flex flex-row  justify-center items-start">
-                  <p className='inline-flex  text-eb-garamond text-[14px] font-bold w-[90px]  text-[#6f3617]'> Pembicara  :  </p>
+                  <p className='inline-flex  text-eb-garamond text-[14px] font-bold w-[90px]  text-[#6f3617] animate-slide-down'> Pembicara  :  </p>
                   <div className='inline-flex flex-col'>
-                    <p className=' inline-flex   text-eb-garamond text-[14px]  font-bold text-[#6f3617]'>
+                    <p className=' inline-flex   text-eb-garamond text-[14px]  font-bold text-[#6f3617] animate-slide-down'>
                       Muhammad Iqbal S. Psi., M. Sc., Ph. D.
                     </p>
-                    <p className=' inline-flex  text-eb-garamond text-[14px] font-bold  text-[#6f3617]'>
+                    <p className=' inline-flex  text-eb-garamond text-[14px] font-bold  text-[#6f3617] animate-slide-down'>
                       Pakar Psikolog , CEO Rumah Konseling
                     </p>
                   </div>
                 </div>
               </div>
 
-              <div className='w-full flex justify-center mt-[10px] items-center'>
+              <div className='w-full flex justify-center mt-[10px] items-center animate-slide-down'>
                 <span className="inline-flex text-[#6f3617]  mt-[10px] text-center font-bold text-eb-garamond text-[22px]">DES</span>
-                <span className="inline-flex bg-[#ec998b] text-[#000] w-[80px] h-[80px] justify-center items-center rounded-full  mt-[10px] text-center font-bold text-eb-garamond text-[30px] mx-[10px]">14</span>
+                <span className="inline-flex bg-[#ec998b] text-[#000] w-[80px] h-[80px] justify-center items-center rounded-full  mt-[10px] text-center font-bold text-eb-garamond text-[30px] mx-[10px] animate-slide-down">14</span>
                 <span className="inline-flex text-[#6f3617]  mt-[10px] text-center font-bold text-eb-garamond text-[22px]">2024</span>
               </div>
 
               <div className='flex justify-center items-center mt-[10px]'>
-                <button onClick={open} className='rounded-2xl text-white bg-[#6f3617] py-[10px] px-[20px] font-bold'>
+                <button onClick={open} className='rounded-2xl text-white bg-[#6f3617] py-[10px] px-[20px] font-bold animate-slide-down'>
                   Buka Undangan
                 </button>
               </div>
             </div>
             {/* bottom */}
-            <div className="flower-right z-30 bottom-0 right-0 w-[30%] rotate-180 absolute  inline-flex justify-end">
-              <img className="w-[100%]" src={flowerOne} alt="" />
+            <div className="animate-slide-left">
+              <div className="flower-right z-30 bottom-0 right-0 w-[100px] rotate-180 absolute  inline-flex justify-end">
+                <img className="w-[100%]" src={flowerOne} alt="" />
+              </div>
             </div>
-            <div className="flower-left z-30 bottom-0 left-0  [transform:rotateX(180deg)] w-[30%] absolute  inline-flex justify-end">
-              <img className="w-[100%]" src={flowerOne} alt="" />
+            <div className="animate-slide-right">
+              <div className="flower-left z-30 bottom-0 left-0  [transform:rotateX(180deg)] w-[100px] absolute  inline-flex justify-end">
+                <img className="w-[100%]" src={flowerOne} alt="" />
+              </div>
             </div>
+
           </section>
 
           <section id="second" ref={secondSectionRef} className="w-full h-screen relative">
-            <div className="flower-right z-30 top-0 right-0 w-[30%] absolute  inline-flex justify-end">
-              <img className="w-[100%]" src={imageFlower} alt="" />
+
+
+            <div className='animate-slide-left'>
+              <div className="flower-right z-30 top-0 right-0 w-[30%] absolute  inline-flex justify-end">
+                <img className="w-[100%]" src={imageFlower} alt="" />
+              </div>
             </div>
-            <div className="flower-left z-30 top-0 left-0 [transform:rotateY(180deg)] w-[30%] absolute  inline-flex justify-end">
-              <img className="w-[100%]" src={imageFlower} alt="" />
+            <div className="animate-slide-right">
+              <div className="flower-left z-30 top-0 left-0 [transform:rotateY(180deg)] w-[30%] absolute  inline-flex justify-end">
+                <img className="w-[100%]" src={imageFlower} alt="" />
+              </div>
             </div>
             {/* top */}
             <img className="w-full h-screen object-cover" src={imageBackground} alt="" />
@@ -134,23 +146,23 @@ function App() {
             </div>
 
             {/* content */}
-            <div className='absolute top-[0] left-[50%] bottom-0 flex flex-col justify-start  items-center w-full [transform:translateX(-50%)] p-[20px]'>
+            <div className='absolute top-[0] left-[50%] bottom-0 flex flex-col justify-start  items-center w-full [transform:translateX(-50%)] px-[10px] pb-[20px] pt-[40px]'>
               <House size={32} color="#b3404a" />
               <p className='text-eb-garamond text-[20px] mt-[10px] text-[#6f3617] font-bold text-center'>SEMINAR PARENTING</p>
-              <p className='text-[#b84c65] text-pinyon-script mt-[20px] text-center font-bold text-[50px]'>
+              <p className='text-[#b84c65] text-pinyon-script mt-[20px] text-center font-bold text-[40px]'>
                 Generasi Strawberry
               </p>
 
-              <p className='text-black   text-center font-bold uppercase text-eb-garamond text-[14px]'>
+              <p className='text-black  text-center font-bold uppercase text-eb-garamond text-[14px]'>
                 "Dibully tak tumbang, dipuji tak terbang"
               </p>
               <p className='text-black   text-center font-bold uppercase text-eb-garamond text-[14px]'>
                 "mendidik Si tangguh, agar tak mudah rapuh"
               </p>
-              <div className='w-full flex justify-center items-center my-[50px]'>
+              <div className='w-full flex justify-center items-center my-[30px]'>
                 <AvatarElement></AvatarElement>
               </div>
-              <div className='flex justify-center items-center  mt-[20px]'>
+              <div className='flex justify-center items-center'>
                 <div className="flex flex-row  justify-center items-start">
 
                   <div className='flex w-full justify-center flex-col'>
@@ -187,7 +199,7 @@ function App() {
               <img className="w-[100%]" src={rain} alt="" />
             </div>
             {/* content */}
-            <div className='absolute top-[0] left-[50%] bottom-0 flex flex-col justify-start pt-[20px] items-center w-full [transform:translateX(-50%)] p-[20px]'>
+            <div className='absolute top-[0] left-[50%] bottom-0 flex flex-col justify-start pt-[20px] items-center w-full [transform:translateX(-50%)] pb-[20px] pt-[40px] px-[20px]'>
               <div className='w-full flex justify-center'>
                 <div className='flex flex-col'>
                   <div className='w-full justify-center items-center flex flex-col'>
@@ -236,7 +248,7 @@ function App() {
                   </div>
                 </div>
                 <div className='w-full flex justify-center items-center mt-[20px]'>
-                  <a href="https://maps.app.goo.gl/Dapz8EcfZdKcYtHR7" target='blank' className=" text-white px-[20px] py-[10px] bg-[#6f3617] rounded-full ">
+                  <a href="https://wa.me/6287860424961?text=Absen%20Kehadiran.%20%0ANama%20Peserta%20:%20%0A" target='blank' className=" text-white px-[20px] py-[10px] bg-[#6f3617] rounded-full ">
                     Konfirmasi Kehadiran
                   </a>
                 </div>
